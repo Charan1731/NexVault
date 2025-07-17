3e2ac9c0dfde25b199cff2b3805240c89f9189b8
Charandeep Reddy K <charan23114@gmail.com>
2025-07-17 13:32:09 +0530

    added navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,18 @@ import { ThemeToggle } from './theme-toggle'
 
 const Navbar = () => {
   return (
+    <div className='fixed top-8 left-8 right-8 border-6 border-black rounded-2xl  z-50 bg-background/50 backdrop-blur-sm dark:border-white'>
     <div className='flex justify-between items-center p-4'>
+        <div className='flex items-center gap-4 pl-5'>
         <h1 className='text-2xl font-bold'>Logo</h1>
+        <p className='text-sm text-muted-foreground'>Your AI-Powered Assistant</p>
+        </div>
         <div className='flex items-center gap-4'>
             <ThemeToggle />
-            <Button variant='outline'>Login</Button>
-            <Button variant='outline'>Register</Button>
+            <Button variant='outline' className='border-4 border-black dark:border-white'>Login</Button>
+            <Button variant='outline' className='border-4 border-black dark:border-white'>Register</Button>
         </div>
+    </div>
     </div>
   )
 }
